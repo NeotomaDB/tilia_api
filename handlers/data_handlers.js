d983ae740f9b6dfa871e856f0682d798887a4705
SimonGoring <simon.j.goring@gmail.com>
2018-04-25 13:55:50 -0700

    Added the code to manage passing in and parsing the functions within the pg database, within the ti namespace.  Needs to manage both the ti and ta namspeaces still.

--- handlers/data_handlers.js
+++ handlers/data_handlers.js
@@ -1,11 +1,11 @@
-//get global database object
-var db = require('../database/pgp_db');
-var pgp = db.$config.pgp;
+// get global database object
+// var db = require('../database/pgp_db')
+// var pgp = db.$config.pgp
 
 // Defining the query functions:
 module.exports = {
-  allfunctions: function (req, res, next) { 
-    var pg_funk = require('../pgfunctions/pgfunction.js');
-    pg_funk.allfunctions(req, res, next); 
+  allfunctions: function (req, res, next) {
+    var pgFunk = require('../pgfunctions/pgfunction.js')
+    pgFunk.allFunctions(req, res, next)
   }
-};
+}
--- pgfunctions/pgfunction.js
+++ pgfunctions/pgfunction.js
@@ -1,34 +1,81 @@
-// Sites query:
+// Postgres functions for Tilia:
 
-const path = require('path');
+const path = require('path')
 
-//get global database object
- var db = require('../database/pgp_db');
-var pgp = db.$config.pgp;
+// get global database object
+var db = require('../database/pgp_db')
+var pgp = db.$config.pgp
 
 // Helper for linking to external query files:
-function sql(file) {
-    const fullPath = path.join(__dirname, file);
-    return new pgp.QueryFile(fullPath, {minify: true});
+function sql (file) {
+  const fullPath = path.join(__dirname, file)
+  return new pgp.QueryFile(fullPath, {minify: true})
 }
 
 // Create a QueryFile globally, once per file:
-const query_func = sql('./fun_query.sql');
- 
-function allfunctions(req, res, next) {
+const queryFunc = sql('./fun_query.sql')
 
-  db.any(query_func)
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all tables'
-        });
-    })
-    .catch(function (err) {
-        return next(err);
-    }) 
+function allFunctions (req, res, next) {
+  console.log(req.query)
+
+  var noParam = Object.keys(req.query).length === 0 && req.query.constructor === Object
+  var method = Object.keys(req.query).includes('method')
+
+  // The call to the documentation JSON object occurs if the user either
+  // enters no parameters, or the term 'method' fails to appear in the
+  // documentation.
+
+  if (noParam | !method) {
+    var dbFuncs = db.any(queryFunc)
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Retrieved all tables'
+          })
+      })
+      .catch(function (err) {
+        return next(err)
+      })
+
+    return dbFuncs
+  } else {
+    var allParams = req.query
+    var sqlMethod = allParams.method
+
+    delete allParams.method
+
+    var sqlCall = 'SELECT * FROM ti.' + sqlMethod + '('
+
+    for (var i = 0; i < Object.keys(allParams).length; i++) {
+      sqlCall = sqlCall + Object.keys(allParams)[i] + ' := ' + allParams[Object.keys(allParams)[i]]
+
+      if (i < Object.keys(allParams).length - 1) {
+        sqlCall = sqlCall + ', '
+      }
+    }
+
+    sqlCall = sqlCall + ')'
+
+    console.log(sqlCall)
+
+    var dbCall = db.any(sqlCall)
+      .then(function (data) {
+        console.log(data)
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Retrieved all tables'
+          })
+      })
+      .catch(function (err) {
+        return next(err)
+      })
+
+    return (dbCall)
+  }
 }
 
-module.exports.allfunctions = allfunctions;
+module.exports.allFunctions = allFunctions
