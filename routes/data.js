5da53b72342f2b87c15cafcc191e30ac8d3fc4bc
Simon <simon.j.goring@gmail.com>
2018-07-23 14:35:16 -0700

    Cleaned up routes.js

--- routes/data.js
+++ routes/data.js
@@ -10,10 +10,6 @@ var router = express.Router()
 
 var handlers = require('../handlers/data_handlers')
 
-router.get('/', (req, res) => {
-  res.send('Hey banana.')
-});
-
 router.get('/retrieve', handlers.allfunctions);
 
 module.exports = router
