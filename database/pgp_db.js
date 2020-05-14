cd09bb26b99592c9513b5c2e2bbb25a61717dc1a
Simon <simon.j.goring@gmail.com>
2020-05-14 13:33:43 -0700

    Adding database handlers & stuff.

--- database/pgp_db.js
+++ database/pgp_db.js
@@ -3,15 +3,20 @@ var promise = require('bluebird');
 const fs = require('fs')
 
 var options = {
-  // Initialization Options
-  promiseLib: promise
+    // Initialization Options
+    promiseLib: promise
 };
 
 const pgp = require('pg-promise')(options);
-const connectpath = '../../node_config/db_connect.json';
 
-if(process.env.NODE_ENV === "development") {
-  connectpath = '../db_connect.json'
+if (process.env.NODE_ENV === "development")
+{
+    var connectpath = '../db_connect.json'
+}
+else
+{
+    var connectpath = './db_connect.json';
+    console.log(process.env.NODE_ENV)
 }
 
 const ctStr = require(connectpath);
@@ -19,11 +24,13 @@ const ctStr = require(connectpath);
 const db = pgp(ctStr);
 
 db.proc('version')
-  .then(data => {
-     console.log(data.version);
-  })
-  .catch(error => {
-    console.log("error connecting");
-  });
+    .then(data =>
+    {
+        console.log(data.version);
+    })
+    .catch(error =>
+    {
+        console.log("error connecting");
+    });
 
 module.exports = db;
