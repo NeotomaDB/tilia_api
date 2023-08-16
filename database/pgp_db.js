f88461ab1726c5165fa8aa9332419935f707ec9a
SimonGoring <goring@wisc.edu>
2023-08-16 13:51:10 -0700

    Trying to do some debugging.

--- database/pgp_db.js
+++ database/pgp_db.js
@@ -21,6 +21,7 @@ const options = {
   error (err, e) {
     var date = new Date()
     // Exclude the big chunky query:
+    console.log(JSON.stringify(err))
     var messageout = { 'error': JSON.stringify(err), 'query': e.query }
     messageout.db = { 'client': e.client.user, 'database': e.client.database, 'host': e.client.host }
     console.log(date.toISOString() + ' ' + JSON.stringify(messageout))
@@ -42,7 +43,8 @@ function dbheader (req) {
     'database': process.env.RDS_DATABASE,
     'password': process.env.RDS_PASSWORD,
     'port': process.env.RDS_PORT,
-    'ssl': true
+    'ssl': true,
+    'query_timeout': 3000
   }
 console.log(out)
   return pgp(out)
