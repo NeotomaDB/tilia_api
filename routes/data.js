9cc64d784851bb49254416512a4b40dcd5ee828f
Simon Goring <simon.j.goring@gmail.com>
2023-11-02 15:38:52 -0700

    Added POST route to `/api`.

--- routes/data.js
+++ routes/data.js
@@ -22,6 +22,7 @@ router.get('/api/logs', handlers.handleLogs)
 
 // Populates the dojo API.  Returns all API endpoints/Postgres functions.
 router.get('/api', handlers.allfunctions)
+router.post('/api', handlers.allfunctions)
 
 // Handles single and batch requests where parameters are passed in the body.
 router.post('/api/update/write', handlers.handlePostMultiUpdate)
