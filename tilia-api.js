de0770037ffb0a228635e9a73c7f78507e855d04
SimonGoring <goring@wisc.edu>
2023-11-06 10:22:09 -0800

    Updated the rate limit to support testing.

--- tilia-api.js
+++ tilia-api.js
@@ -34,7 +34,7 @@ console.log = function () {
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 2 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false // Disable the `X-RateLimit-*` headers
 })
